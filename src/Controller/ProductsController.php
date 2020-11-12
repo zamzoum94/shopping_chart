ce99c2be16db53bdb9eebe2eedea384495e453e8
zamzoum94 <mzemzemi9@gmail.com>
2020-11-12 10:14:12 +0100

    authentification and flash messages

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -4,27 +4,31 @@ namespace App\Controller;
 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use App\Entity\Product;
+use App\Form\ProductType as FormProduct;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class ProductsController extends AbstractController
 {
     private $em = null;
     public function __construct(EntityManagerInterface $em){
         $this->em = $em;
+
     }
 
     /**
-     * @Route("/products", name="products", methods={"GET"})
+     * @Route("/", name="products", methods={"GET"})
      */
     public function index(ProductRepository $prod): Response
     {
-        return $this->render('layouts/index.html.twig', [
+        return $this->render('layouts/products.html.twig', [
             'products' => $prod->findAll()
         ]);
     }
@@ -45,6 +49,7 @@ class ProductsController extends AbstractController
         if($form->isSubmitted() && $form->isValid()){
             $this->em->persist($product);
             $this->em->flush();
+            $this->addFlash("success", "Product succefully added");
             return $this->redirectToRoute("products");
         }
 
@@ -56,21 +61,29 @@ class ProductsController extends AbstractController
     /**
      * @Route("/product/update/{id}", name="product_update", methods={"GET", "PUT"})
      */
-     public function update(Product $prod, Request $request):Response{
-         $form = $this->createFormBuilder($prod)
-             ->setMethod("PUT")
-             ->add("name")
-             ->add("description")
-             ->add("price")
-             ->getForm();
+     public function update(Product $prod,
+                            Request $request) : Response{
+         $form = $this->createForm(FormProduct::class, $prod, ["method" => "PUT"]);
 
          $form->handleRequest($request);
          if($form->isSubmitted()){
-             dd($request);
+             $this->em->flush();
+             $this->addFlash("success", "Product succefully Updated");
+             return $this->redirectToRoute("products");
          }
          return $this->render('layouts/update_product.html.twig',[
              "form" => $form->createView(),
              "product" => $prod
          ]);
+     }
+
+    /**
+     * @Route("/product/delete/{id}", name="product_delete", methods={"DELETE"})
+     */
+     public function delete(Product $product, Request $request) : Response{
+         $this->em->remove($product);
+         $this->em->flush();
+         $this->addFlash("danger", "Product succefully removed");
+         return $this->redirectToRoute("products");
      }
 }
